7d2808b4e9f78bff062bd229e57f22f79ebd09de
samkomo <ilabadmin@ilabadmins-iMac-14.local>
2014-10-21 20:20:23 +0300

    Settings

---
+++ TrailerTrail/ViewControllers/SettingsViewController.h
@@ -0,0 +1,15 @@
+//
+//  SettingsViewController.h
+//  TrailerTrail
+//
+//  Created by ilabadmin on 10/21/14.
+//  Copyright (c) 2014 Maginnovate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BaseViewController.h"
+
+@interface SettingsViewController : BaseViewController
+@property (weak, nonatomic) IBOutlet UIScrollView *myScrollView;
+
+@end
---
+++ TrailerTrail/ViewControllers/SettingsViewController.m
@@ -0,0 +1,52 @@
+//
+//  SettingsViewController.m
+//  TrailerTrail
+//
+//  Created by ilabadmin on 10/21/14.
+//  Copyright (c) 2014 Maginnovate. All rights reserved.
+//
+
+#import "SettingsViewController.h"
+
+@interface SettingsViewController ()
+
+@end
+
+@implementation SettingsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    CGRect bounds = [self.view bounds];
+    self.view.frame = bounds;
+    self.myScrollView.contentSize = CGSizeMake(self.myScrollView.frame.size.width, 716);
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
